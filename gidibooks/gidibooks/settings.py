aa8c22f0ae0693d2e119207bde939b88e691bea5
luckyadogun <meetluckyadogun@gmail>
2021-04-06 22:07:46 +0100

    static settings added

--- gidibooks/gidibooks/settings.py
+++ gidibooks/gidibooks/settings.py
@@ -65,7 +65,6 @@ AUTH_USER_MODEL = "users.User"
 
 
 PASSWORD_HASHERS = [
-    "django.contrib.auth.hashers.Argon2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
     "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
@@ -99,3 +98,9 @@ USE_TZ = True
 
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
